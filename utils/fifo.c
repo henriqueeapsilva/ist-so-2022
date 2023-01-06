18ec9abbe3c92d18080d994f6abc646f026b60d0
Fábio Neto <fabiogvdneto@gmail.com>
2023-01-06 12:29:29 +0000

    Finished fifo.c and created fifo.h

--- utils/fifo.c
+++ utils/fifo.c
@@ -5,50 +5,65 @@
 
 // Helper function to send messages
 // Retries to send whatever was not sent in the beginning
-void send_msg(int tx, char const *str, int fill) {
+void fifo_send_msg(int fd, char const *str) {
     size_t len = strlen(str);
     size_t written = 0;
 
     while (written < len) {
-        ssize_t ret = write(tx, str + written, len - written);
-        if (ret < 0) {
-            fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-
-        written += ret;
+        written += fifo_write(fd, str + written, len - written);
     }
 }
 
-void fifo_make(char *name, int mode) {
-
-}
-
-int fifo_open(char *name, int mode) {
-    if (open(name, mode) == -1) {
-        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+void fifo_make(char *name, mode_t mode) {
+    if (mkfifo(name, mode) == -1) {
+        fprintf(stderr, "Could not create fifo: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
 
-void fifo_write_msg(int fhandler) {
-
+int fifo_open(char *name, int flags) {
+    int fhandler = open(name, flags);
+    
+    if (fhandler == -1) {
+        fprintf(stderr, "Could not open fifo: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    return fhandler;
 }
 
-void fifo_read_msg(int fhandler) {
+ssize_t fifo_write(int fd, const void *buffer, size_t count) {
+    ssize_t ret = write(fd, buffer, count);
 
+    if (ret < 0) {
+        fprintf(stderr, "Could not write to fifo: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    return ret;
+}
+
+ssize_t fifo_read(int fd, const void *buffer, size_t count) {
+    ssize_t ret = read(fd, buffer, count);
+
+    if (ret < 0) {
+        fprintf(stderr, "Could not read from fifo: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    return ret;
 }
 
 void fifo_close(int fhandler) {
     if (close(fhandler) == -1) {
-        fprintf(stderr, "[ERR]: close failed: %s\n", strerror(errno));
+        fprintf(stderr, "Could not close fifo: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
 
 void fifo_unlink(char *name) {
-    if (unlink(name) != 0 && errno != ENOENT) {
-        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", name, strerror(errno));
+    if ((unlink(name) != 0) && (errno != ENOENT)) {
+        fprintf(stderr, "Could not unlink fifo (%s): %s\n", name, strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
---
+++ utils/fifo.h
@@ -0,0 +1,17 @@
+#ifndef _FIFO_H_
+#define _FIFO_H_
+
+#include <stdlib.h>
+
+void fifo_send_msg(int fd, const char *buffer);
+
+void fifo_make(const char *name, mode_t mode);
+void fifo_unlink(const char *name);
+
+void fifo_open(const char *name, int flags);
+void fifo_close(int fd);
+
+ssize_t fifo_read(int fd, const void *buffer, size_t count);
+ssize_t fifo_write(int fd, const void *buffer, size_t count);
+
+#endif
