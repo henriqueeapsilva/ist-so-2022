728a58aea98513e4e87aee1939f85ae47cac9674
Fábio Neto <fabiogvdneto@gmail.com>
2023-01-08 20:46:24 +0000

    Fixed pub.c

--- publisher/pub.c
+++ publisher/pub.c
@@ -15,47 +15,52 @@ int main(int argc, char **argv) {
         return EXIT_SUCCESS;
     }
 
-    // load channels
+    // create channel
 
     create_channel(argv[2], 0640);
 
-    int fregister = open_channel(argv[1], O_WRONLY);
-    int fsession = open_channel(argv[2], O_WRONLY);
-
     // send request
 
-    uint8_t code = 1;
-    memwrite_to_channel(fregister, &code);
-    strwrite_to_channel(fregister, argv[2], 256);
-    strwrite_to_channel(fregister, argv[3], 32);
+    {
+        int fregister = open_channel(argv[1], O_WRONLY);
+
+        uint8_t code = 1;
+        memwrite_to_channel(fregister, &code);
+        strwrite_to_channel(fregister, argv[2], 256);
+        strwrite_to_channel(fregister, argv[3], 32);
+
+        close_channel(fregister);
+    }
 
     // publish messages
 
-    char msg[LENGTH];
-    char c = getchar();
-    int i = 0;
+    {
+        int fsession = open_channel(argv[2], O_WRONLY);
 
-    while (c != EOF) {
-        if (c == '\n') {
-            memset(msg + i, '\0', LENGTH - i);
-            memwrite_to_channel(fsession, msg);
-            continue;
+        char msg[LENGTH];
+        char c = getchar();
+        int i = 0;
+
+        while (c != EOF) {
+            if (c == '\n') {
+                memset(msg + i, '\0', LENGTH - i);
+                memwrite_to_channel(fsession, msg);
+            } else if ((i == LENGTH - 1)) {
+                do {
+                    c = getchar();
+                } while ((c != EOF) && (c != '\n'));
+                continue;
+            }
+
+            c = getchar();
+            i++;
         }
 
-        if ((i == LENGTH - 1)) {
-            do {
-                c = getchar();
-            } while ((c != EOF) && (c != '\n'));
-            continue;
-        }
-
-        c = getchar();
+        close_channel(fsession);
     }
 
-    // unload channels
+    // delete channel
 
-    close_channel(fregister);
-    close_channel(fsession);
     delete_channel(argv[2]);
 
     return EXIT_SUCCESS;
