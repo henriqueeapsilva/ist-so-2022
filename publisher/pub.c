b54f7d2057d27795a5e1844501eeb6009ecc4f53
Fábio Neto <fabiogvdneto@gmail.com>
2023-01-08 19:08:10 +0000

    Publisher can now send requests and publish messages.

--- publisher/pub.c
+++ publisher/pub.c
@@ -1,42 +1,63 @@
-#include "../utils/logging.h"
+#include "utils/logging.h"
+#include "utils/channel.h"
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../utils/fifo.h"
+#include <stdint.h>
+#include <string.h>
+
+#define LENGTH 1024
+
 int main(int argc, char **argv) {
-
-    // create session fifo
-    fifo_make(argv[2], 0640);
-
-    int fregister = fifo_open(argv[1], O_WRONLY);
-
-    char msg[sizeof(u_int8_t) + 256 + 32];
-
-    sprintf(msg, "%d", 1);      // insert request code
-    strncpy(msg+1, argv[2], 256);    // insert session pipe name
-    strncpy(msg+1+256, argv[4], 32); // insert box name
-
-    fifo_send_msg(fregister, msg);
-    fifo_close(fregister);
-
-    int fsession = fifo_open(argv[2], O_RDONLY);
-    char buffer[1024];
-    int readed = fifo_read(fsession,buffer, 1024);
-    // we have to decide on the mbroker which message is sent as it's ok to read or not
-    /*
-     * code to confirm if it's ready
-     * */
-    fifo_close(fsession);
-
-    int fs = fifo_open(argv[2], O_WRONLY);
-    char buf[1024];
-    int c;
-    while((c=getchar()) != EOF){
+    if (argc < 4) {
+        fprintf(stderr, "usage: pub <register_pipe_name> <pipe_name> <box_name>\n");
+        return EXIT_SUCCESS;
     }
 
-    fifo_close(fs);
+    // load channels
 
-    fprintf(stderr, "usage: pub <register_pipe_name> <pipe_name> <box_name>\n");
-    return -1;
+    create_channel(argv[2], 0640);
+
+    int fregister = open_channel(argv[1], O_WRONLY);
+    int fsession = open_channel(argv[2], O_WRONLY);
+
+    // send request
+
+    uint8_t code = 1;
+    memwrite_to_channel(fregister, &code);
+    strwrite_to_channel(fregister, argv[2], 256);
+    strwrite_to_channel(fregister, argv[3], 32);
+
+    // publish messages
+
+    char msg[LENGTH];
+    char c = getchar();
+    int i = 0;
+
+    while (c != EOF) {
+        if (c == '\n') {
+            memset(msg+i, '\0', LENGTH-i);
+            memwrite_to_channel(fsession, msg);
+            continue;
+        }
+
+        if ((i == LENGTH-1)) {
+            do {
+                c = getchar();
+            } while ((c != EOF) && (c != '\n'));
+            continue;
+        }
+
+        c = getchar();
+    }
+
+    // unload channels
+
+    close_channel(fregister);
+    close_channel(fsession);
+    delete_channel(argv[2]);
+
+    return EXIT_SUCCESS;
 }
