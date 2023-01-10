909ed1f55b1d2d0abf9f201d5795ecfbf26a49c1
Fábio Neto <fabiogvdneto@gmail.com>
2023-01-10 12:53:25 +0000

    Code is now more compact and readable.

--- manager/manager.c
+++ manager/manager.c
@@ -14,113 +14,77 @@ static void print_usage() {
             "   manager <register_pipe_name> <pipe_name> list\n");
 }
 
-int eval_request(int argc, char **argv);
-void update_box(uint8_t code, char *reg_pipe_path, char *pipe_path, char *box_name);
-void list(char *reg_pipe_path, char *pipe_path);
-
-int main(int argc, char **argv) {
-    if (!eval_request(argc, argv)) {
-        print_usage();
-    }
-
-    return EXIT_SUCCESS;
-}
-
-int eval_request(int argc, char **argv) {
+static int eval_request(int argc, char **argv) {
     if (argc < 4)
         return 0;
 
     if (!strcmp(argv[3], "create")) {
-        if (argc < 5)
-            return 0;
-
-        update_box(3, argv[1], argv[2], argv[4]);
-        return 1;
+        return (argc >= 5) * 3;
     }
 
     if (!strcmp(argv[3], "remove")) {
-        if (argc < 5)
-            return 0;
-
-        update_box(5, argv[1], argv[2], argv[4]);
-        return 1;
+        return (argc >= 5) * 5;
     }
 
     if (!strcmp(argv[3], "list")) {
-        list(argv[1], argv[2]);
-        return 1;
+        return 7;
     }
 
     return 0;
 }
 
-void update_box(uint8_t code, char *reg_pipe_path, char *pipe_path, char *box_name) {
-    // create channel
+int main(int argc, char **argv) {
+    uint8_t code = eval_request(argc, argv);
 
-    create_channel(pipe_path, 0640);
-
-    // send request
-
-    send_quick_message(reg_pipe_path, code, pipe_path, box_name);
-
-    // receive response
-
-    int fd = open_channel(pipe_path, O_RDONLY);
-
-    int32_t error_code;
-    char error_message[1024];
-
-    assert(receive_code(fd) == ++code);
-    receive_content(fd, code, &error_code, error_message);
-
-    if (error_code) {
-        fprintf(stdout, "ERROR %s\n", error_message);
-    } else {
-        fprintf(stdout, "OK\n");
+    if (!code) {
+        print_usage();
+        return EXIT_SUCCESS;
     }
 
-    close_channel(fd);
+    create_channel(argv[2], 0640);
 
-    // delete channel
+    if (code == 7) { /* list boxes request */
+        send_quick_message(argv[1], code, argv[2]);
+    } else { /* create/remove box request */
+        send_quick_message(argv[1], code, argv[2], argv[4]);
+    }
 
-    delete_channel(pipe_path);
-}
+    int fd = open_channel(argv[2], O_RDONLY);
+    assert(receive_code(fd) == ++code);
 
-void list(char *reg_pipe_path, char *pipe_path) {
-    // create channel
+    if (code == 8) { /* list boxes response */
+        int32_t errcode;
+        char errmessage[1024];
 
-    create_channel(pipe_path, 0640);
+        receive_content(fd, code, &errcode, errmessage);
 
-    // send request
+        if (errcode) {
+            fprintf(stdout, "ERROR %s\n", errmessage);
+        } else {
+            fprintf(stdout, "OK\n");
+        }
+    } else { /* create/remove box response */
+        uint8_t code = 7;
+        uint8_t last;
+        char box_name[32];
+        uint64_t box_size;
+        uint64_t n_publishers;
+        uint64_t n_subscribers;
 
-    send_quick_message(reg_pipe_path, 7, pipe_path);
+        receive_content(fd, code, &last, box_name, &box_size, &n_publishers, &n_subscribers);
 
-    // receive response
-
-    int fd = open_channel(pipe_path, O_RDONLY);
-
-    uint8_t code = 7;
-    uint8_t last;
-    char box_name[32];
-    uint64_t box_size;
-    uint64_t n_publishers;
-    uint64_t n_subscribers;
-
-    assert(receive_code(fd) == code);
-    receive_content(fd, code, &last, box_name, &box_size, &n_publishers, &n_subscribers);
-
-    if (!*box_name) {
-        fprintf(stdout, "NO BOXES FOUND\n");
-    } else {
-        while (!last) {
-            assert(receive_code(fd) == code);
-            receive_content(fd, code, &last, box_name, &box_size, &n_publishers, &n_subscribers);
+        if (!*box_name) {
+            fprintf(stdout, "NO BOXES FOUND\n");
+        } else {
+            while (!last) {
+                assert(receive_code(fd) == code);
+                receive_content(fd, code, &last, box_name, &box_size, &n_publishers, &n_subscribers);
+            }
         }
     }
 
     close_channel(fd);
+    delete_channel(argv[2]);
 
-    // delete channel
-
-    delete_channel(pipe_path);
+    return EXIT_SUCCESS;
 }
